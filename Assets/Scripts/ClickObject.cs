dca6ff8338ec4ed835b9ed0df9b5b0d89a014e7f
tpangili <tpangili@ucsc.edu>
2024-03-01 13:16:25 -0800

    New ClickObject script

--- Assets/Scripts/ClickObject.cs
+++ Assets/Scripts/ClickObject.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ClickObject : MonoBehaviour
 {
-    public List<Color> colorList = new List<Color>();
-    public int currentColor = 0; // 0 = Red, 1 = Green, 2 = Blue.
+    public List<Material> colorList = new List<Material>();
+    public int currentColor = 0; // 0 = Red, 1 = Green, 2 = Blue, 3 = Default/White.
+    public PhysicMaterial bouncyMaterial;
+    public PhysicMaterial defaultMaterial;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,27 @@ public class ClickObject : MonoBehaviour
             {
                 //Debug.Log("OBJECT HIT");
                 hit.collider.gameObject.GetComponent<MeshRenderer>().material = colorList[currentColor];
+                switch(currentColor) 
+                {
+                    case 0:
+                        // Red
+                        hit.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+                        break;
+                    case 1:
+                        // Green
+                        hit.collider.gameObject.GetComponent<Collider>().sharedMaterial = bouncyMaterial;
+                        break;
+                    case 2:
+                        // Blue
+                        hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                        break;
+                    default:
+                        // White
+                        hit.collider.gameObject.GetComponent<Collider>().sharedMaterial = defaultMaterial; // Reverses green
+                        hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = true; // Reverses blue
+                        hit.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; // Reverses red
+                        break;
+                }
             }
         }
     }
